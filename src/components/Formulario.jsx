b259e8619b1b25de85e2b499eac0f361ce5ce265
DiegoDiem <ldmg.9@hotmail.com>
2024-01-07 18:47:01 -0600

    add changes

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -34,7 +34,7 @@ const Formulario = ({cliente, cargando}) => {
             let respuesta
             if(cliente.id){
                 //Editando un registro
-                const url = `http://localhost:4000/clientes/${cliente.id}`
+                const url = `${import.meta.env.VITE_API_URL}/${cliente.id}`
 
                 respuesta = await fetch(url, {
                     method:'PUT',
@@ -45,7 +45,7 @@ const Formulario = ({cliente, cargando}) => {
                 })
             }else{
                 // Nuevo Registro
-                const url = 'http://localhost:4000/clientes'
+                const url = import.meta.env.VITE_API_URL
 
                 respuesta = await fetch(url, {
                     method:'POST',
